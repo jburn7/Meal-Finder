4027af3ea9557768f3504e75cd0a4f888db10834
Dong-Seop Eah <deah@bu.edu>
2019-03-19 23:27:35 -0400

    call brewery api

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-var express = require('express')
-var app = express()
+// const path = require("path")
+const express = require('express')
+const request = require('request')
+const app = express()
+
+// app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
-  res.send('this is our project')
+  if (Object.keys(req.query).length == 0) {
+    res.send('After the url type /?id=1234 to display the brewery name. Any id from 1 to 8029 will work.')
+  } 
+  else {
+    request('https://api.openbrewerydb.org/breweries/'+req.query.id, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body)
+        res.send(info.name)
+      }
+    })
+  }
 })
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 app.listen(app.get('port'), function () {
   console.log('Node app running at localhost:' + app.get('port'))
