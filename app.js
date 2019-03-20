23553e1f6f0d1c50acb0895a4c3b1675986d3f21
Dong-Seop Eah <deah@bu.edu>
2019-03-19 23:45:13 -0400

    edit response text

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.get('/', function (req, res) {
     res.send('After the url type /?id=1234 to display the brewery name. Any id from 1 to 8029 will work.')
   } 
   else {
-    request('https://api.openbrewerydb.org/breweries/'+req.query.id, function (error, response, body) {
+    request('https://api.openbrewerydb.org/breweries/' + req.query.id, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var info = JSON.parse(body)
-        res.send(info.name)
+        res.send('Brewery Name: ' + info.name + '<br>' + 'Website: ' + info.website_url)
       }
     })
   }
