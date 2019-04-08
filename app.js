eabddc4711d69cc3ad8047fab369c27fe972d1fd
Dong-Seop Eah <deah@bu.edu>
2019-04-08 00:11:07 -0400

    Remove MongoDB connection (crashes Heroku)

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Testing will remove later, connects to MongoDB Atlas and inserts a document
-const uri = "mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@cs411-vrnjt.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    collection.insertOne({ connectSuccess: 'Success!' })
-    client.close();
-});
+// const uri = "mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@cs411-vrnjt.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     collection.insertOne({ connectSuccess: 'Success!' })
+//     client.close();
+// });
 
 app.get('/', function (req, res) {
     res.render('index')
