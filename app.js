dd72bbcc7ead512ab588e444552c61de55f75970
Dong-Seop Eah <deah@bu.edu>
2019-04-07 23:15:28 -0400

    Test connection to MongoDB Atlas

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 const dotenv = require('dotenv')
 const request_promise = require('request-promise-lite')
 const async = require('async')
+const MongoClient = require('mongodb').MongoClient
 
 dotenv.config();
 
@@ -14,6 +15,16 @@ app.set('port', (process.env.PORT || 5000))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Testing will remove later, connects to MongoDB Atlas and inserts a document
+const uri = "mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@cs411-vrnjt.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    collection.insertOne({ connectSuccess: 'Success!' })
+    client.close();
+});
 
 app.get('/', function (req, res) {
     res.render('index')
